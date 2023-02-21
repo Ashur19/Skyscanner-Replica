a84eb0d0a3e4e87f59afe837fa5d8832d26992bf
Ashur19 <iglesiasjota1911@gmail.com>
2023-02-21 12:46:44 +0100

    Third Commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,18 +8,22 @@ import menu from "../Img/Menu.png"
 const Header = () => {
 
     return (
-        <div className="Homepage">
-            <div className="Homepage-nav">
-                <a href="https://www.skyscanner.es" className="Homepage-nav-title"><img src={logo}/></a>
-                <div className="Homepage-nav-menus">
-                    <button className="Homepage-nav-menus-region"><img src={region}/></button>
-                    <button className="Homepage-nav-menus-login"><img src={login}/></button>
-                    <button className="Homepage-nav-menus-menu"><img src={menu}/></button>
+        <div className="homepage">
+            <div className="homepage-nav">
+                <a href="https://www.skyscanner.es" className="homepage-nav-title"><img src={logo}/></a>
+                <div className="homepage-nav-menus">
+                    <button className="homepage-nav-menus-region"><img src={region}/></button>
+                    <button className="homepage-nav-menus-login"><img src={login}/></button>
+                    <button className="homepage-nav-menus-menu"><img src={menu}/></button>
                 </div>
             </div>
             <h1>Busca rápido en tus sitios de viajes favorito</h1>
-            <div className="Homepage-searchcontrols">
-                <div></div>
+            <div className="homepage-searchcontrols">
+                <div className="homepage-searchcontrols-from">
+                    <label>
+
+                    </label>
+                </div>
             </div>
         </div>
     )
